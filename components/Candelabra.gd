8b40c18e97197ad1d95ac0858b62f69f90508750
Rico Apon <rico.apon@gmail.com>
2024-06-15 18:04:20 +0200

    Add candelabras

---
+++ components/Candelabra.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+# Emits "true" if the lamp is now on, and "false" if it is now off.
+signal light(bool)
+
+var contains_player = false
+@onready var point_light = $PointLight2D
+
+func _on_area_2d_body_entered(body):
+	if body.name != "Player":
+		return
+	contains_player = true
+
+func _on_area_2d_body_exited(body):
+	if body.name != "Player":
+		return
+	contains_player = false
+
+func _unhandled_input(event):
+	if !contains_player:
+		return
+	
+	if event.is_action_pressed("action"):
+		point_light.enabled = !point_light.enabled
+		light.emit(point_light.enabled)
--- maps/Start.gd
+++ maps/Start.gd
@@ -9,3 +9,16 @@ func _on_teleporter_a_1_teleport():
 
 func _on_teleporter_a_2_teleport():
 	teleport_player.emit($TeleporterA1.position)
+
+
+var nr_of_lightened_candelabras = 2
+func _on_candelabra_light(enabled):
+	if enabled:
+		nr_of_lightened_candelabras += 1
+	else:
+		nr_of_lightened_candelabras -= 1
+	
+	if nr_of_lightened_candelabras == 0:
+		$HiddenGem.visible = true
+	else:
+		$HiddenGem.visible = false
